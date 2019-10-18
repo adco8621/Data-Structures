029a9730d70ed311f1415c49c8d96839af600dfa
AddisonConzet <adco8621@colorado.edu>
2019-10-18 13:05:38 -0600

    Finalized class functions

--- Structs/Hw/HW6/MovieTree.cpp
+++ Structs/Hw/HW6/MovieTree.cpp
@@ -8,6 +8,64 @@ using namespace std;
 
 // MovieNode: node struct that will be stored in the MovieTree BST
 
+
+
+void deleteTree(MovieNode *node){
+  if (node == NULL) return;
+
+  if (node->left != NULL){
+    deleteTree(node->left);
+  }
+  if (node->right != NULL){
+    deleteTree(node->right);
+  }
+
+  /* TODO then delete the node */
+  node->left = NULL;
+  node->right = NULL;
+  delete node;
+}
+
+void traversePrintTree(MovieNode *node){
+  if (node == NULL) return;
+
+  if (node->left != NULL){
+    traversePrintTree(node->left);
+  }
+  cout << "Movie: " << node->title << " " << node->rating << endl;
+  if (node->right != NULL){
+    traversePrintTree(node->right);
+    }
+}
+
+void queryHelper(MovieNode *node, int year, float rating){
+  if (node == NULL){
+    return;
+  }
+
+  if ((node->year >= year) && (node->rating >= rating)){
+    cout << node->title << "(" << node->year << ") " << node->rating << endl;
+  }
+
+  queryHelper(node->left, year, rating);
+  queryHelper(node->right, year, rating);
+  return;
+}
+
+void ratingHelper(MovieNode *node, float &rating, int &count){
+  if (node == NULL){
+    return;
+  }
+
+  rating += node->rating;
+  count++;
+  ratingHelper(node->right, rating, count);
+  ratingHelper(node->left, rating, count);
+
+  return;
+
+}
+
 MovieTree::MovieTree() {
   root = NULL;
 }
@@ -63,12 +121,12 @@ void MovieTree::findMovie(string title) {
       node = node->right;
     }
     else if (node->title == title){
-      cout ​<<​​"Movie Info:"​​<<​ endl;
-      cout ​<<​​"=================="​​<<​ endl;
-      cout ​<<​​"Ranking:"​​<< node->ranking <<​ endl;
-      cout ​<<​​"Title  :"​​<< node->title <<​ endl;
-      cout ​<<​​"Year   :"​​<< node->year <<​ endl;
-      cout ​<<​​"rating :"​​<< node->rating <<​ endl;
+      cout << "Movie Info:" << endl;
+      cout << "==================" <<  endl;
+      cout << "Ranking:" << node->ranking << endl;
+      cout << "Title  :" << node->title << endl;
+      cout << "Year   :" << node->year << endl;
+      cout << "rating :" << node->rating << endl;
       break;
     }
     else {
@@ -84,50 +142,26 @@ void MovieTree::queryMovies(float rating, int year) {
     cout << "Tree is Empty. Cannot query Movies" << endl;
     return;
   }
-  cout​ << ​"Movies that came out after " ​<< year <<​ " with rating atleast " ​<< rating <<​ ":"​ << ​endl​;
-  queryHelper(root);
+  cout << "Movies that came out after " << year << " with rating at least " << rating << ":" << endl;
+  queryHelper(root, year, rating);
   return;
 }
 
 void MovieTree::averageRating() {
-  //write your code
-}
+  
+  int count = 0;
+  float rating = 0, avg = 0;
 
-void deleteTree(MovieNode *node){
-  if (node == NULL) return;
+  ratingHelper(root, rating, count);
 
-  if (node->left != NULL){
-    deleteTree(node->left);
+  if (count){
+    avg = rating/count;
+    cout << "Average rating:" << avg << endl;
+    return;
   }
-  if (node->right != NULL){
-    deleteTree(node->right);
-  }
 
-  /* TODO then delete the node */
-  node->left = NULL;
-  node->right = NULL;
-  delete node;
-}
+   cout << "Average rating:0.0" << endl;
 
-void traversePrintTree(MovieNode *node){
-  if (node == NULL) return;
-
-  if (node->left != NULL){
-    traversePrintTree(node->left);
-  }
-  cout << "Movie: " << node->title << " " << node->rating << endl;
-  if (node->right != NULL){
-    traversePrintTree(node->right);
-    }
-}
-
-void queryHelper(MovieNode *node){
-  if (node == NULL){
-    return
-  }
-  cout << node->title << "(" << node->year << ")" << node->rating << endl;
-
-  queryHelper(node->left);
-  queryHelper(node->right);
   return;
+
 }
